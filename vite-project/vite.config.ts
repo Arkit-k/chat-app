69183091f444c3f74cfba0cf9dee1f90b09db57e
arkit_k <arkitkarmokar007@gmail.com>
2025-01-29 12:40:06 +0530

    frontend commit

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    allowedHosts: ['5abe-160-238-79-11.ngrok-free.app'],
+  }
+})
